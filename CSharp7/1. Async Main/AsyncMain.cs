9f306de98abbac514727c75175eae9bdbb927d34
Marcin Zajkowski <m.zajkowski@sicc.pl>
2017-10-29 23:51:50 +0100

    1. Async Main example

---
+++ CSharp7/1. Async Main/AsyncMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharp7._1._Async_Main
+{
+    class AsyncMain
+    {
+        static void Main()
+        {
+            //var seriousTalkBetweenTwoExperienceBeconEaters = await JoinMemoriesFromLongLastingWhiskyNightsWithDangAsync();
+
+            //Console.WriteLine(seriousTalkBetweenTwoExperienceBeconEaters);
+        }
+
+        private static async Task<string> JoinMemoriesFromLongLastingWhiskyNightsWithDangAsync()
+        {
+            const string apiUrl = "https://baconipsum.com/api/";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+
+                var mySentenceApiCall = await client.GetAsync("?type=meat-and-filler&sentences=1&format=text");
+                mySentenceApiCall.EnsureSuccessStatusCode();
+
+                var dangsResponse = await client.GetAsync("?type=meat-and-filler&sentences=5&format=text");
+                dangsResponse.EnsureSuccessStatusCode();
+
+                var mySentence = await mySentenceApiCall.Content.ReadAsStringAsync();
+                var dangsSentence = await dangsResponse.Content.ReadAsStringAsync();
+                
+                return $"\n\n[Marcin]: {mySentence}\n\n[Anthony]: {dangsSentence}\n\n";
+            }
+        }
+    }
+}
