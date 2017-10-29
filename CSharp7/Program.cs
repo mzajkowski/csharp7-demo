1c4df9549ea4fd00e5381f2e65d758a9712f9db5
Marcin Zajkowski <m.zajkowski@sicc.pl>
2017-10-29 23:50:51 +0100

    Basic demo setup
    Thanks Jon Skeet!

---
+++ CSharp7/Program.cs
@@ -0,0 +1,12 @@
+﻿using CSharp7.Util;
+
+namespace CSharp7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ApplicationChooser.Run(typeof(Program), args);
+        }
+    }
+}
