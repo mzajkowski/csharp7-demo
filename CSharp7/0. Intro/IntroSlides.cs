979a06ffeec960a3acb6c2daa2b3884c3d6fe0f6
Marcin Zajkowski <m.zajkowski@sicc.pl>
2017-11-01 15:37:00 +0100

    WIP

---
+++ CSharp7/0. Intro/IntroSlides.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace CSharp7._0._Intro
+{
+    class IntroSlides
+    {
+        static void Main(string[] args)
+        {
+            const string introSlidesUrl = "https://slides.com/zajkowskimarcin/";
+
+            var psi = new ProcessStartInfo("chrome.exe")
+            {
+                Arguments = introSlidesUrl
+            };
+            Process.Start(psi);
+        }
+    }
+}
